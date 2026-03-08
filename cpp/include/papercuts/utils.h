a82e005f08be41ba1ca66f43d09e414fa84def0b
ilthraim <millerek5@gmail.com>
2026-03-08 17:02:48 -0400

    Added tokenToInt

--- cpp/include/papercuts/utils.h
+++ cpp/include/papercuts/utils.h
@@ -1,10 +1,12 @@
 #pragma once
 
 #include "slang/util/BumpAllocator.h"
+#include "slang/parsing/Token.h"
 #include <string>
 #include <string_view>
 #include <span>
 
 namespace papercuts {
     std::string_view persistString(slang::BumpAllocator& alloc, const std::string& str);
+    int tokenToInt(const slang::parsing::Token& token);
 }
--- cpp/src/utils.cpp
+++ cpp/src/utils.cpp
@@ -1,9 +1,21 @@
 #include "papercuts/utils.h"
+#include <charconv>
 
 namespace papercuts {
     std::string_view persistString(slang::BumpAllocator& alloc, const std::string& str) {
         std::span<const char> src(str.data(), str.size());
         std::span<char> persistent = alloc.copyFrom(src);
         return {persistent.data(), persistent.size()};
+    }
+
+    int tokenToInt(const slang::parsing::Token& token) {
+        auto text = token.valueText();
+        int value;
+        auto result = std::from_chars(text.data(), text.data() + text.size(), value);
+
+        if (result.ec != std::errc()) {
+            return 0;
+        }
+        return value;
     }
 }
