1226a310d68792a7738bb9170fc443e1da9cbaa3
ilthraim <millerek5@gmail.com>
2026-03-16 22:34:05 -0400

    Added helper methods

--- cpp/include/papercuts/papercuts.h
+++ cpp/include/papercuts/papercuts.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <iostream>
 #include <memory>
+#include <optional>
+#include <span>
 #include <string>
 #include <string_view>
 #include <unordered_set>
@@ -7,20 +10,97 @@
 #include "slang/syntax/AllSyntax.h"
 #include "slang/syntax/SyntaxNode.h"
 #include "slang/syntax/SyntaxVisitor.h"
+#include "slang/parsing/TokenKind.h"
 
 using namespace slang::syntax;
+using namespace slang::parsing;
 
 namespace papercuts {
+class ASTPrinter  : public SyntaxVisitor<ASTPrinter> {
+public:
+    void handle(const DataDeclarationSyntax& node) {
+        this->visitDefault(node);
+    }
+};
+
+class TestRewriter : public SyntaxRewriter<TestRewriter> {
+public:
+    void handle(const DataDeclarationSyntax& node) {
+        const auto& modifiers = node.modifiers;
+        std::cout << "Found a data declaration with " 
+                  << modifiers.size() << " modifiers!" << std::endl;
+        
+        for (size_t i = 0; i < modifiers.size(); i++) {
+            std::cout << "  Modifier[" << i << "]: " 
+                      << modifiers[i].valueText() << std::endl;
+
+            this->removeToken(modifiers, i);
+        }
+    }
+};
+
 class ModuleNameRewriter : public SyntaxRewriter<ModuleNameRewriter> {
+    private:
+        std::string newName; // Store the new name we want to give to the module
     public:
         void handle(const ModuleHeaderSyntax&);
+        std::shared_ptr<SyntaxTree> renameModule(const std::shared_ptr<SyntaxTree>, std::string);
+};
+
+// Mark: Base Rewriter
+template<typename TDerived>
+class PapercutsRewriter : public SyntaxRewriter<TDerived> {
+protected:
+    using SyntaxRewriter<TDerived>::makeToken;
+
+    Token makeSemicolon(std::span<const Trivia> trivia = {}) { return makeToken(TokenKind::Semicolon, ";", trivia); }
+    Token makeOpenBrace(std::span<const Trivia> trivia = {}) { return makeToken(TokenKind::OpenBrace, "{", trivia); }
+    Token makeCloseBrace(std::span<const Trivia> trivia = {}) { return makeToken(TokenKind::CloseBrace, "}", trivia); }
+    Token makeEquals(std::span<const Trivia> trivia = {}) { return makeToken(TokenKind::Equals, "=", trivia); }
+    Token makeOpenBracket(std::span<const Trivia> trivia = {}) { return makeToken(TokenKind::OpenBracket, "[", trivia); }
+    Token makeCloseBracket(std::span<const Trivia> trivia = {}) { return makeToken(TokenKind::CloseBracket, "]", trivia); }
+    Token makeColon(std::span<const Trivia> trivia = {}) { return makeToken(TokenKind::Colon, ":", trivia); }
+
+    template<typename TNode>
+    SeparatedSyntaxList<TNode> makeSeparatedList(std::span<TNode* const> nodes,
+                                                 std::optional<Token> separator = std::nullopt) {
+        if (nodes.empty())
+            return SeparatedSyntaxList<TNode>{std::span<TokenOrSyntax>{}};
+
+        slang::SmallVector<TokenOrSyntax> buffer;
+        const size_t count = separator ? (nodes.size() * 2 - 1) : nodes.size();
+        buffer.reserve(count);
+
+        for (size_t i = 0; i < nodes.size(); ++i) {
+            buffer.push_back(nodes[i]);
+            if (separator && i + 1 < nodes.size())
+                buffer.push_back(separator->deepClone(this->alloc));
+        }
+
+        return SeparatedSyntaxList<TNode>(buffer.copy(this->alloc));
+    }
+
+    template<typename TNode>
+    SyntaxList<TNode> makeSyntaxList(std::span<TNode* const> nodes) {
+        if (nodes.empty())
+            return SyntaxList<TNode>{std::span<TNode*>{}};
+
+        slang::SmallVector<TNode*> buffer;
+        buffer.reserve(nodes.size());
+        for (TNode* node : nodes)
+            buffer.push_back(node);
+
+        return SyntaxList<TNode>(buffer.copy(this->alloc));
+    }
+private:
 };
 
 // MARK: BitShrink
-class BitShrinkRewriter : public SyntaxRewriter<BitShrinkRewriter> {
+class BitShrinkRewriter : public PapercutsRewriter<BitShrinkRewriter> {
 private:
     std::string_view nodeToShrink; // Store the name of the current node we want to shrink
-    int newWidth; // Store the new width we want to shrink to
+    const DeclaratorSyntax* currentNode; // Store the current DeclaratorSyntax node we want to shrink
+    std::string newName; // Store the new name we want to give to the node we're shrinking
     std::unordered_map<const DeclaratorSyntax*, int> widthMap; // Map to store the width of each DeclaratorSyntax node
     bool done = false; // Flag to indicate if we've already shrunk bits in the current tree
 public:
@@ -39,7 +119,7 @@ public:
 };
 
 //MARK: Ternary
-class TernaryRemover : public SyntaxRewriter<TernaryRemover> {
+class TernaryRemover : public PapercutsRewriter<TernaryRemover> {
 private:
     std::unordered_set<const ConditionalExpressionSyntax*> nodesToChange;
     bool done = false;
@@ -58,7 +138,7 @@ public:
 };
 
 // MARK: If
-class IfRemover : public SyntaxRewriter<IfRemover> {
+class IfRemover : public PapercutsRewriter<IfRemover> {
 private:
     std::unordered_set<const ConditionalStatementSyntax*> nodesToChange;
     bool done = false;
--- cpp/include/papercuts/utils.h
+++ cpp/include/papercuts/utils.h
@@ -9,4 +9,9 @@
 namespace papercuts {
     std::string_view persistString(slang::BumpAllocator& alloc, const std::string& str);
     int tokenToInt(const slang::parsing::Token& token);
+
+    template<class T>
+    std::span<T, 1> singleSpan(T& t) {
+        return std::span<T, 1>(&t, 1);
+    }
 }
